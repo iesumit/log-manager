ba6cbcf418a500c8b185a7a7b70092b410a1b1b2
Sumit S <ssinha@dius.com>
2019-11-29 09:35:14 +1100

    Update log parsing logic

--- ship-logs/lib.js
+++ ship-logs/lib.js
@@ -5,20 +5,29 @@ const parse = require("./parse");
 const cloudWatchEncoding = "base64";
 const payloadEncoding = "utf8";
 
-const logger = require('logzio-nodejs').createLogger({
+const logger = require("logzio-nodejs").createLogger({
   token: process.env.logzio_token,
-  protocol: 'https',
+  protocol: "https",
   host: process.env.logzio_host,
-  port: process.env.logzio_port,
-})
+  port: process.env.logzio_port
+});
 const processAll = function(logGroup, logStream, logEvents) {
   const lambdaVersion = parse.lambdaVersion(logStream);
   const functionName = parse.functionName(logGroup);
   for (const logEvent of logEvents) {
     const log = parse.logMessage(logEvent);
+    if (log) {
+      log.logStream = logStream;
+      log.logGroup = logGroup;
+      log.functionName = functionName;
+      log.lambdaVersion = lambdaVersion;
+      log.fields = log.fields || {};
+      log.type = "cloudwatch";
+      log.token = process.env.logzio_token;
+    }
     logger.log(log);
-    logger.sendAndClose();
   }
+  logger.sendAndClose();
 };
 
 const decode = function(data) {
